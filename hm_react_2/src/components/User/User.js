a2c16979e9d2b9bb05271375a31ecdbac81ab84e
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-04-29 15:14:49 +0300

    dz_react_2

--- hm_react_2/src/components/User/User.js
+++ hm_react_2/src/components/User/User.js
@@ -4,10 +4,15 @@ import {useState} from 'react';
 
 export  default function User (props){
     const {name,age,isMarried,address}=props
+    const del=()=>{
+
+    }
+
     return(
         <div className={"user"}>
             <h3>{name}</h3>
-            <p>{age}-{isMarried}-{address.city}</p>
+            <p>{age}-{isMarried.toString()}-{address.city}</p>
+            <button onClick={del}>del</button>
         </div>
     )
 }

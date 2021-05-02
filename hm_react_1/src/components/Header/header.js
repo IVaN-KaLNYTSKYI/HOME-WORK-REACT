06abed0a6b8181f7f394c4e97aa547acbbd93dcc
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-05-02 20:03:28 +0300

    create(ade,css)

--- hm_react_1/src/components/Header/header.js
+++ hm_react_1/src/components/Header/header.js
@@ -1,10 +1,11 @@
 import './Header.css'
 function Header(props){
-    const{text,del}=props
+    const{text,age,id,del}=props
     return(
     <div className={"text"}>
-        {text}
-        <button onClick={()=>del(text)}>del</button>
+        <span>{text}</span>
+        <span>{age}</span>
+        <button className={"btn-del"} onClick={()=>del(id)}>del</button>
        {/* <button onClick={()=>del(text)}>del</button>*/}
     </div>
     )
--- hm_react_1/src/components/Main/main.js
+++ hm_react_1/src/components/Main/main.js
@@ -1,42 +1,57 @@
 import './main.css'
-import {useState,useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import Header from "../Header/header";
-function Main(){
-let [text,setText]=useState("");
-let [arr,setArr]=useState(JSON.parse(localStorage.getItem("arr"))||[]);
-useEffect(()=>{
-    localStorage.setItem("arr",JSON.stringify(arr))
-})
-const add=(e)=>{
-    e.preventDefault();
-    setArr([...arr,{text}])
-    setText("")
-    console.log(arr)
-}
-const change=(e)=>{
-    text=e.target.value
-    setText(text)
-}
-const del=(text)=>{
-    setArr(arr.filter((value => value.text!==text)))
-}
-    return(
-        <div>
-            {
-                    arr.map((item,index )=>
+
+function Main() {
+    let [text, setText] = useState("");
+    let [age, setAge] = useState("");
+    let [arr, setArr] = useState(JSON.parse(localStorage.getItem("arr")) /*|| []*/);
+    useEffect(() => {
+        localStorage.setItem("arr", JSON.stringify(arr))
+    })
+    const add = (e) => {
+        e.preventDefault();
+        setArr([...arr, {text,age,id:Date.now()}])
+       /* setText("")
+        setAge("")*/
+    }
+    const change = (e) => {
+        text = e.target.value
+        setText(text)
+    }
+    const changetwo = (e) => {
+        age = e.target.value
+        setAge(age)
+    }
+    const del = (id) => {
+        setArr(arr.filter((value => value.id !== id)))
+    }
+    return (
+        <div className={'main'}>
+            <div className={"form"}>
+                <form>
+                    <input
+                        onChange={change}
+                        placeholder={"Name"}
+                    />
+                    <input
+                        onChange={changetwo}
+                        placeholder={"Age"}
+                    />
+                    <button onClick={add}>Add</button>
+                </form>
+            </div>
+            <div className={"infa"}>
+                {
+                    arr.map((item, index) =>
                         <Header
-                        key={index}
+                            key={index}
                             {...item}
                             del={del}
                         />
                     )
-            }
-          <form >
-              <input
-                  onChange={change}
-              />
-             <button onClick={add}>Add</button>
-          </form>
+                }
+            </div>
         </div>
     )
 }

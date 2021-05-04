a525470ddc04d60982dec8f2ca62c115e88b2b06
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-05-05 01:58:01 +0300

    dz_3_react(fetch) and router

--- hm_react_3/src/App.js
+++ hm_react_3/src/App.js
@@ -2,12 +2,34 @@
 import './App.css';
 import Users from "./components/Users/Users";
 import Posts from "./components/Posts/Posts";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div >
-     <Users/>
-     <Posts/>
+        <Router>
+            <ul>
+                <li>
+                    <Link to="/users">Users</Link>
+                </li>
+                <li>
+                    <Link to="/posts">Posts</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path={"/users"}>
+                    <Users/>
+                </Route>
+                <Route path={"/posts"}>
+                    <Posts/>
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }

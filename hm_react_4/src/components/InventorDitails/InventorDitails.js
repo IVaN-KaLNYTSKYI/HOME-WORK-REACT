fa3f4f62173152489feecc8fc579f042cc3e6b0a
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-05-11 23:12:21 +0300

    getInventorItem

--- hm_react_4/src/components/InventorDitails/InventorDitails.js
+++ hm_react_4/src/components/InventorDitails/InventorDitails.js
@@ -4,13 +4,13 @@ export default function InventorDetails({item}){
     let [user, setUser] = useState(null);
     useEffect(()=>{
         getInventorItem(item.match.params.id).then(value => setUser({...value}))
-    })
-    const {title}=user
+    },[])
+    console.log(user)
     return(
         <div>
             {
                 user&&<div>
-                    {title}
+                    {user.data.title}
                 </div>
             }
         </div>
